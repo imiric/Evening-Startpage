7c52aa16303627b70991be068d4eeabff035bd1e
Ivan Mirić <1009277+imiric@users.noreply.github.com>
2026-01-31 12:14:22 +0100

    feat: simplify, remove deps

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,16 +30,6 @@ function startTime() {
   var time = setTimeout(function(){ startTime() }, 60000);
 }
 
-// You can add your own random quotes and stuff here
-const quotes = [
-  'Hello, world...',
-  'Goodbye world sudo rm -rf *',
-  'This is a random quote'
-];
-document.getElementById("header-quote").innerText = quotes[
-  Math.floor(Math.random() * quotes.length)
-];
-
 document.addEventListener('click', function (event) {
 
 	// If the clicked element doesn't have the right selector, bail
@@ -60,12 +50,9 @@ const $s = {
 // [command character]: ['search url', 'title']
 function engines () {
   return {
-	g: ['https://google.com/search?q=', 'Google'],
-	i: ['https://google.com/search?tbm=isch&q=', 'Google Images'],
-	yt: ['https://youtube.com/results?search_query=', 'Youtube'],
-	s: ['https://stackoverflow.com/search?q=', 'Stackoverflow'],
-	a: ['https://web.archive.org/web/*/', 'Archive'],
-	w: ['https://en.wikipedia.org/w/index.php?search=', 'Wikipedia'],
+	  k: ['https://www.kagi.com/search?q=', 'Kagi'],
+	  a: ['https://web.archive.org/web/*/', 'Archive'],
+	  w: ['https://en.wikipedia.org/w/index.php?search=', 'Wikipedia'],
   };
 }
 
@@ -73,8 +60,8 @@ var search  = $s.qS('#search'),
     input   = $s.qS('#search input[type="text"]'),
     engines = engines();
 
-for (var key in engines)
-  $s.qS('.search-engines').innerHTML += `<li><p title="${engines[key][1]}">!${key}</p></li>`;
+// for (var key in engines)
+//   $s.qS('.search-engines').innerHTML += `<li><p title="${engines[key][1]}">!${key}</p></li>`;
 
 document.onkeypress = (e) => {
     if (e.key == 's')
@@ -86,7 +73,7 @@ document.onkeypress = (e) => {
     search.onkeyup = (e) => {
       let args   = e.target.value.split(' '),
           prefix = args[0],
-          engine = engines['g'][0], // the default engine (google in this case)
+          engine = engines['k'][0], // the default engine (Kagi in this case)
           str    = 0;
 
       $s.qA('.search-engines li p').forEach((eng) => {
@@ -107,4 +94,4 @@ document.onkeypress = (e) => {
     };
 };
 
-document.getElementById("container").addEventListener("DOMContentLoaded", startTime());
+document.addEventListener("DOMContentLoaded", startTime);
